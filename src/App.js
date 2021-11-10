25826b32dbbc6077ce8358f077e8f1faeb5b5eb1
Leonid Semeniuk <leo.sam@ukr.net>
2021-11-10 23:08:27 +0200

    Add structure and modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return <div className='App'>Hello</div>;
+  return (
+    <div className='App'>
+      <div className='App__one'>Hello</div>
+    </div>
+  );
 }
 
 export default App;
