1ea5039657c8b48bcc252000791ba8209ead2e8d
Leonid Semeniuk <leo.sam@ukr.net>
2021-11-12 00:32:09 +0200

    add errorboundary, theme, normalize

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+// import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
+// import store from './store/store';
+// import ErrorBoundary from './components/Error/ErrorBoundary/ErrorBoundary';
 import App from './App';
-import './index.css';
+import reportWebVitals from './reportWebVitals';
+import './theme/styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Provider store={store}> */}
+    <BrowserRouter>
+      {/* <ErrorBoundary> */}
+      <App />
+      {/* </ErrorBoundary> */}
+    </BrowserRouter>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
