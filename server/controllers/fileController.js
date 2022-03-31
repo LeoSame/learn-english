1f55b064d577322774e2652a63e8c2cfcecc161b
Leonid Semeniuk <leo.sam@ukr.net>
2022-04-01 02:48:39 +0300

    -

--- server/controllers/fileController.js
+++
@@ -1,6 +0,0 @@
-const User = require('../models/User');
-const Word = require('../models/Word');
-
-class WordController {}
-
-module.exports = new WordController();
--- server/services/fileService.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const File = require('../models/Word');
-const config = require('config');
-
-class FileService {}
-
-module.exports = new FileService();
